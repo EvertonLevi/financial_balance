20eed09726a61ca6106fc0aac8809cb1e32e9ea8
Levi <everton.levi@hotmail.com>
2021-02-18 23:41:44 -0300

    requisição 'Total de despesas' e link com api

--- frontend/src/components/Pages/Home/index.js
+++ frontend/src/components/Pages/Home/index.js
@@ -10,78 +10,79 @@ import { FaWallet, FaCashRegister, FaMoneyBillWave } from 'react-icons/fa'
 import api from '../../../services/api'
 
 export default function Home({ }) {
-    const [launch, setLaunch] = useState([])
+	const [launch, setLaunch] = useState([])
+	const [countLaunch, setCountLaunch] = useState()
 
-    async function getLaunch() {
-        const response = await api.get('launchs')
-            .then((res) => {
-                setLaunch(res)
-                console.log("RESPONSE: ", res)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }
+	async function getLaunch() {
+		const response = await api.get('launchs')
+		let num = 0;
+		for (let index = 0; index < response.data.length; index++) {
+			num = parseFloat(response.data[index].launch + num);
+		}
+		setLaunch(num)
+	}
 
-    useEffect(() => {
-        getLaunch()
-    }, [])
+	useEffect(() => {
+		getLaunch()
+		// TODO
+
+	}, [])
 
 
-    return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            minHeight: '100vh',
-        }}>
-            <div>
-                <p>Olá usuário</p>
-                <img style={{ borderRadius: '50px' }} src={user}></img>
-            </div>
-            <div
-                style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    height: '100vh'
-                }}>
-                <div style={{ backgroundColor: 'gray', width: '20%', height: 'auto' }}>
-                    <ButtonDelete>DELETE</ButtonDelete>
-                    <ButtonEdit>EDIT</ButtonEdit>
-                    <ButtonSave>SAVE</ButtonSave>
-                </div>
-                <div style={{
-                    width: '80%'
-                }}>
-                    <div style={{
-                        display: 'flex', flexDirection: 'row',
-                        justifyContent: 'space-between',
-                        alignItems: 'center'
-                    }}>
-                        <CardRecipe>
-                            <div>
-                                <strong style={{ color: 'white', fontSize: '2em' }}>R$2900,00</strong>
-                                <p style={{ color: 'white', marginTop: '.4em' }}>Total de Receitas</p>
-                            </div>
-                            <FaCashRegister style={{ color: 'white', fontSize: '3em' }} />
-                        </CardRecipe>
-                        <CardExpenditure >
-                            <div>
-                                <strong style={{ color: 'white', fontSize: '2em' }}>R$ {console.log("launch: ", launch)}</strong>
-                                <p style={{ color: 'white', marginTop: '.4em' }}>Total de Despesas</p>
-                            </div>
-                            <FaWallet style={{ color: 'white', fontSize: '3em' }} />
-                        </CardExpenditure>
-                        <CardCashier>
-                            <div>
-                                <strong style={{ color: 'white', fontSize: '2em' }}>R$400,00</strong>
-                                <p style={{ color: 'white', marginTop: '.4em' }}>Caixa atual</p>
-                            </div>
-                            <FaMoneyBillWave style={{ color: 'white', fontSize: '3em' }} />
-                        </CardCashier>
-                    </div>
+	return (
+		<div style={{
+			display: 'flex',
+			flexDirection: 'column',
+			minHeight: '100vh',
+		}}>
+			<div>
+				<p>Olá usuário</p>
+				<img style={{ borderRadius: '50px' }} src={user}></img>
+			</div>
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'row',
+					height: '100vh'
+				}}>
+				<div style={{ backgroundColor: 'gray', width: '20%', height: 'auto' }}>
+					<ButtonDelete>DELETE</ButtonDelete>
+					<ButtonEdit>EDIT</ButtonEdit>
+					<ButtonSave>SAVE</ButtonSave>
+				</div>
+				<div style={{
+					width: '80%'
+				}}>
+					<div style={{
+						display: 'flex', flexDirection: 'row',
+						justifyContent: 'space-between',
+						alignItems: 'center'
+					}}>
+						<CardRecipe>
+							<div>
+								<strong style={{ color: 'white', fontSize: '2em' }}>R$2900,00</strong>
+								<p style={{ color: 'white', marginTop: '.4em' }}>Total de Receitas</p>
+							</div>
+							<FaCashRegister style={{ color: 'white', fontSize: '3em' }} />
+						</CardRecipe>
+						<CardExpenditure >
+							<div>
+								<strong style={{ color: 'white', fontSize: '2em' }}>{launch.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</strong>
+								<p style={{ color: 'white', marginTop: '.4em' }}>Total de Despesas</p>
+							</div>
+							<FaWallet style={{ color: 'white', fontSize: '3em' }} />
+						</CardExpenditure>
+						<CardCashier>
+							<div>
+								<strong style={{ color: 'white', fontSize: '2em' }}>R$400,00</strong>
+								<p style={{ color: 'white', marginTop: '.4em' }}>Caixa atual</p>
+							</div>
+							<FaMoneyBillWave style={{ color: 'white', fontSize: '3em' }} />
+						</CardCashier>
+					</div>
 
-                </div>
-            </div>
-        </div >
-    )
+				</div>
+			</div>
+		</div >
+	)
 }
