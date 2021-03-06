30b709fa67eac368245e86bcbaca59b219a80e4b
Levi <everton.levi@hotmail.com>
2021-03-06 00:31:54 -0300

    iniciando estrutura de login com jwt

--- backend/server.py
+++ backend/server.py
@@ -1,17 +1,43 @@
 from datetime import datetime
 from random import randrange
 
-from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_jwt_extended import (JWTManager, create_access_token,
+                                get_jwt_identity, jwt_required)
+from flask_restful import Api, Resource
 from sqlalchemy import create_engine
 
 db_connect = create_engine('sqlite:///db.db')
 app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = "super-secret"
+jwt = JWTManager(app)
 CORS(app)
 api = Api(app)
 
+
 class User(Resource):
+
+    @app.route("/login", methods=["POST"])
+    def login():
+        conn = db_connect.connect()
+        id = randrange(0, 1001, 2)
+        name = request.json['name']
+        senha = request.json['senha']
+        email = request.json['email']
+        access_token = create_access_token(identity=name)
+        # conn.execute("insert into user values('{0}', '{1}', '{2}', '{3}')".format(
+        #     id, name, senha, email))
+        # query = conn.execute("select * from user order by id desc limit 1")
+        # result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+        # return jsonify(result)
+        return jsonify(access_token=access_token)
+
+    @app.route("/protegido", methods=["GET"])
+    @jwt_required()
+    def protected():
+        current_user = get_jwt_identity()
+        return jsonify(logged_in_as=current_user), 200
 
     def post(self):
         conn = db_connect.connect()
@@ -80,6 +106,7 @@ class LaunchById(Resource):
 api.add_resource(User, '/user')
 api.add_resource(Launchs, '/launchs')
 api.add_resource(LaunchById, '/launchs/<id>')
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
